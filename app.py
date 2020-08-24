02b3335e530eb03628f24c9b4b9e4d3c50fff65a
Lucca Bombo <lucca.bombo@hotmail.com>
2020-08-24 18:56:37 +0000

    added custom style and commentary on code

--- app.py
+++ app.py
@@ -11,19 +11,25 @@ mongo = PyMongo(app)
 
 #---------------------Recipes--------------------------------------------
 
+
 @app.route('/')
 def home():
+    """Renders the home.html template"""
     return render_template('home.html')
 
 
 @app.route('/recipes')
 def recipes():
+    """Renders the recipes.html template, finds the recipes and
+       categories collection in Mongo"""
     return render_template('recipes.html', recipes=mongo.db.recipes.find(),
                            categories=mongo.db.categories.find())
 
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
+    """Finds the recipes collection and refines the results
+       based on the recipes category name"""
     query = request.form.get("query")
     recipes = mongo.db.recipes.find({"$text": {"$search": query}})
     return render_template('recipes.html', recipes=recipes,
@@ -32,12 +38,16 @@ def search():
 
 @app.route('/add_recipe')
 def add_recipe():
+    """Renders the add_recipe.html template and find all the
+       categories names on the MongoDB"""
     return render_template('add_recipe.html',
                            categories=mongo.db.categories.find())
 
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
+    """Finds the recipes collection, inserts one recipe after form is
+       filled and returns the user to the recipes.html page"""
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('recipes'))
@@ -45,6 +55,9 @@ def insert_recipe():
 
 @app.route('/insert_review/<recipe_id>', methods=['POST'])
 def insert_review(recipe_id):
+    """Finds the recipe_id, the reviews collection and inserts one
+       review after form is filled.
+       After it redirects the user to the recipe.html page"""
     recipe_id = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     reviews = mongo.db.reviews
     reviews.insert_one(request.form.to_dict())
@@ -53,11 +66,12 @@ def insert_review(recipe_id):
 
 @app.route('/show_recipe/<recipe_id>')
 def show_recipe(recipe_id):
+    """Finds the recipe and displays the recipe_page based on its _id  """
     # find the recipe with the id
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     # get the ingredients from it and split the string into a list
     recipe_ing = recipe['recipe_ingredients'].split('/')
-    # get the cooking method from it and split the string into a list
+    # get the cooking method from it and split the string into paragraphs
     recipe_cook = recipe['recipe_prepare_method'].split('/')
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("recipe_page.html", recipe=the_recipe,
--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,16 +1,6 @@
-function sendMail(contactForm) {
-    emailjs.send("l", "recipe_blog", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "recipe_blog": contactForm.recipeblogconten.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
-}
+emailjs.sendForm('l', 'recipe_blog', 'contactForm')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
